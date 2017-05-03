b620e2a9b7241d94228b0de2a7a9edcb2e07a572
caihongwei <caihongwei@mochui.com>
2017-05-03 19:47:58 +0800

    todomvc mainsection

---
+++ redux-demo/todomvc/src/components/MainSection.js
@@ -0,0 +1,10 @@
+/**
+ * Created by caihongwei on 03/05/2017.
+ */
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+import Footer from './Footer';
+import { SHOW_ACTIVE, SHOW_ALL, SHOW_COMPLETED } from '../constants/TodoFilters';
+
+
---
+++ redux-demo/todomvc/src/constants/TodoFilters.js
@@ -0,0 +1,7 @@
+/**
+ * Created by caihongwei on 03/05/2017.
+ */
+
+export const SHOW_ALL = "show_all";
+export const SHOW_COMPLETED = "show_completed";
+export const SHOW_ACTIVE = "show_active";
